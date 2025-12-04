57b1500d09ae26d1ddcffd6a247280c4db086686
Inky-developer <inky@mailbox.org>
2025-12-04 01:39:30 +0100

    Add smoke test to the assembler

--- src/java/assembler.rs
+++ src/java/assembler.rs
@@ -179,3 +179,35 @@ impl class::ClassData {
         self.add_constant(class::ConstantPoolEntry::String { string_index })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::java::assembler::{
+        Assembler, ConstantValue, FieldDescriptor, Instruction, MethodDescriptor,
+    };
+
+    #[test]
+    fn test_hello_world() {
+        let mut assembler = Assembler::new("Helloworld".to_string());
+        assembler.add_all([
+            Instruction::GetStatic {
+                class_name: "java/lang/System".to_string(),
+                name: "out".to_string(),
+                field_type: FieldDescriptor("Ljava/io/PrintStream;".to_string()),
+            },
+            Instruction::LoadConstant {
+                value: ConstantValue::String("Hello World!".to_string()),
+            },
+            Instruction::InvokeVirtual {
+                class_name: "java/io/PrintStream".to_string(),
+                name: "println".to_string(),
+                method_type: MethodDescriptor("(Ljava/lang/String;)V".to_string()),
+            },
+            Instruction::ReturnNull,
+        ]);
+
+        let class_data = assembler.assemble();
+
+        insta::assert_debug_snapshot!(class_data);
+    }
+}
